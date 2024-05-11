360a864e93a6a3b6743c5362dfd0f97a46be3e69
seol <tjftmd7@gmail.com>
2024-05-11 11:07:10 +0900

    add section4

---
+++ spring-core/section4/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,34 @@
+package hello.core;
+
+import hello.core.Order.OrderService;
+import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //구성정보를 담당한다는 의미
+public class AppConfig {
+    @Bean //이를 이용하면 스프링 컨테이너에 등록
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
+    public static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
